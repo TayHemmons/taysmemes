06952e0cdfee369e3b53843adaa42ddb00ef9756
kenneththomas <kennethwoo11@gmail.com>
2018-01-07 16:29:25 -0500

    i finished it

--- dkrap.py
+++ dkrap.py
@@ -11,7 +11,10 @@ introLyrics=['so they\'re finally here','performing for you','if you know the wo
 chorusLyrics=['D','K','Donkey Kong','D','K','Donkey Kong is here']
 dkLyrics=['he\'s the leader of the bunch','you know him well','he\'s finally here','to kick some tail','his coconut gun','fires in spurts','if he shoots ya','it\'s gonna hurt','he\'s bigger','faster','and stronger too','he\'s the first member','of the DK crew','HUH!']
 tinyLyrics=['this kong\'s got style','so listen up dudes','she can shrink in size','to suit her mood','she\'s quick and nimble','when she needs to be','she can float through the air','and climb up trees','if you choose her','you\'ll not choose wrong','with a skip and a hop','she\'s one cool kong','HUH!']
-diddyLyrics=['he\'s back again','and about time too','and this time','he\'s in the mood']
+lankyLyrics=['he has no style','he has no grace','this kong has a funny face','he can handstand','when he needs to','and stretch his arms out','just for you','inflate himself','just like a balloon','this crazy kong','just digs this tune','HUH!']
+diddyLyrics=['he\'s back again','and about time too','and this time','he\'s in the mood','he can fly real high with his jetpack on','with his pistols out','he\'s one tough kong','he\'ll make you smile','when he plays his tune','but kremlings beware','cause he\'s after you','HUH!']
+chunkyLyrics=['finally','he\'s here for you','it\'s the last member','of the dk crew','this kong\'s so strong','it isn\'t funny','can make a kremling cry out','for his mummy','can pick up a boulder','with relative ease','makes crushing rocks','such a breeze','he may move slow','he can\'t jump high','but this kong\'s','one hell of a guy','HUH!']
+outroLyrics=['walnuts','peanuts','pineapple smells','grapes','melons','oranges','and coconut shells']
 
 def intro():
   for line in introLyrics:
@@ -20,11 +23,10 @@ def intro():
 
 def chorus():
   for line in chorusLyrics:
+    print(line)
     if line == "D":
-      print(line)
       d(.5)
     else:
-      print(line)
       d(1)
 
 def dkVerse():
@@ -37,6 +39,30 @@ def tinyVerse():
     print(line)
     d(1)
 
+def lankyVerse():
+  for line in lankyLyrics:
+    print(line)
+    d(1)
+
+def diddyVerse():
+  for line in diddyLyrics:
+    print(line)
+    d(1)
+
+def chunkyVerse():
+  for line in chunkyLyrics:
+    print(line)
+    d(1)
+
+fasterwords=['grapes','melons','oranges']
+
+def outroVerse():
+  for line in outroLyrics:
+    print(line)
+    if any(fasterword in line for fasterword in fasterwords):
+      d(.5)
+    else:
+      d(1)
 
 
 #play the song!
@@ -46,3 +72,16 @@ dkVerse()
 chorus()
 tinyVerse()
 chorus()
+lankyVerse()
+chorus()
+diddyVerse()
+chorus()
+chunkyVerse()
+chorus()
+#im not writing a function for this one line
+print('come on cranky, take it to the fridge')
+d(2)
+outroVerse()
+print('oh yeah')
+d(1)
+outroVerse()
