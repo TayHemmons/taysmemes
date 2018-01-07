05c3849941c3319769249c49c6241f41dff86802
taylor@staybreez.xyz <taylor@staybreez.xyz>
2018-01-06 20:08:58 -0500

    tiny verse

--- dkrap.py
+++ dkrap.py
@@ -7,10 +7,11 @@ def d(x):
   time.sleep(x)
 
 
-introLyrics=['so we\'re finally here','performing for you','if you know the words','you can join in too','put your hands together','if you want to clap','as we take you through','this monkey rap','HUH!']
+introLyrics=['so they\'re finally here','performing for you','if you know the words','you can join in too','put your hands together','if you want to clap','as we take you through','this monkey rap','HUH!']
 chorusLyrics=['D','K','Donkey Kong','D','K','Donkey Kong is here']
 dkLyrics=['he\'s the leader of the bunch','you know him well','he\'s finally here','to kick some tail','his coconut gun','fires in spurts','if he shoots ya','it\'s gonna hurt','he\'s bigger','faster','and stronger too','he\'s the first member','of the DK crew','HUH!']
-
+tinyLyrics=['this kong\'s got style','so listen up dudes','she can shrink in size','to suit her mood','she\'s quick and nimble','when she needs to be','she can float through the air','and climb up trees','if you choose her','you\'ll not choose wrong','with a skip and a hop','she\'s one cool kong','HUH!']
+diddyLyrics=['he\'s back again','and about time too','and this time','he\'s in the mood']
 
 def intro():
   for line in introLyrics:
@@ -31,10 +32,17 @@ def dkVerse():
     print(line)
     d(1)
 
+def tinyVerse():
+  for line in tinyLyrics:
+    print(line)
+    d(1)
+
 
 
 #play the song!
 intro()
 chorus()
 dkVerse()
+chorus()
+tinyVerse()
 chorus()
