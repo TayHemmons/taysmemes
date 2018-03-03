ac34474d6eff3ba1004af9e72e2a9b6d918400f9
tayhemmons <tayhemmons@protonmail.com>
2018-03-03 09:53:47 -0500

    bentley

---
+++ instagram/bentley.rb
@@ -0,0 +1,15 @@
+#i did not write this. you can feed individual instagram urls into this script to download pictures. i want to improve it to automatically grab the images.
+
+require 'nokogiri'
+require 'open-uri'
+
+ARGV.each_with_index do |arg, index|
+  doc = Nokogiri::HTML(open(arg))
+  doc.xpath("//meta[@property='og:image']/@content").each do |attr|
+    url = attr.value
+    open(index.to_s + '.jpg', 'wb') do |file|
+      file << open(url).read
+      puts 'Downloaded ' + arg + ' to ' + index.to_s + '.jpg'
+    end
+  end
+end
