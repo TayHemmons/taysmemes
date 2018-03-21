2b929ffbcbb0da6396d6059455cb3964fe30a055
kenneththomas <kennethwoo11@gmail.com>
2018-03-21 18:54:06 -0400

    quotes can change up to a dollar. wow

--- quoter/script.js
+++ quoter/script.js
@@ -1,12 +1,19 @@
-function quoteTest(product, price)
+function quoteTest(product, baseprice)
 {
-    document.getElementById("quote").innerHTML = product + ' - ' + price;
     console.log('rfq: ' + product)
-    if (isNaN(price)){
-        console.log('alert: invalid quote ' + price + ' for: '+ product)
+    if (isNaN(baseprice)){
+        console.log('alert: invalid quote ' + baseprice + ' for: '+ product);
+        document.getElementById("quote").innerHTML = product + ' - ' + baseprice;
         document.getElementById("buy").disabled = true;
         document.getElementById("sell").disabled = true;
     } else {
+        var volatility = parseFloat(Math.random().toFixed(2))
+        if (Math.random() >= 0.5){
+            var price2 = baseprice + volatility
+            } else {
+                var price2 = baseprice - volatility
+                }
+        document.getElementById("quote").innerHTML = product + ' - ' + price2;
         document.getElementById("buy").disabled = false;
         document.getElementById("sell").disabled = false;
     }
