4b3e75cd5b1bd16c10828944402424887b675a64
tayhemmons <tayhemmons@protonmail.com>
2018-03-21 18:18:04 -0400

    add buy sell drop buttons but only the drop button does something right now

--- quoter/script.js
+++ quoter/script.js
@@ -1,4 +1,18 @@
-    function quoteTest(product, price)
-    {
-        document.getElementById("quote").innerHTML = product + ' - ' + price;
+function quoteTest(product, price)
+{
+    document.getElementById("quote").innerHTML = product + ' - ' + price;
+    console.log('rfq: ' + product)
+    if (isNaN(price)){
+        console.log('alert: invalid quote ' + price + ' for: '+ product)
+        document.getElementById("buy").disabled = true;
+        document.getElementById("sell").disabled = true;
+    } else {
+        document.getElementById("buy").disabled = false;
+        document.getElementById("sell").disabled = false;
     }
+}
+function dropQuote()
+{
+    document.getElementById("quote").innerHTML = 'No Active Quote'
+    console.log('dropped quote')
+}
