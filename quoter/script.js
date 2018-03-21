73e55f72e68afdf77747c0d88f5624ef10b231fa
kenneththomas <kennethwoo11@gmail.com>
2018-03-21 19:46:34 -0400

    u can buy and theres a table tay plz make it look better

--- quoter/script.js
+++ quoter/script.js
@@ -3,7 +3,8 @@ function quoteTest(product, baseprice)
     console.log('rfq: ' + product)
     if (isNaN(baseprice)){
         console.log('alert: invalid quote ' + baseprice + ' for: '+ product);
-        document.getElementById("quote").innerHTML = product + ' - ' + baseprice;
+        document.getElementById("qproduct").innerHTML = product;
+        document.getElementById("quote").innerHTML = 'ERROR';
         document.getElementById("buy").disabled = true;
         document.getElementById("sell").disabled = true;
     } else {
@@ -13,13 +14,26 @@ function quoteTest(product, baseprice)
             } else {
                 var price2 = baseprice - volatility
                 }
-        document.getElementById("quote").innerHTML = product + ' - ' + price2;
+        document.getElementById("qproduct").innerHTML = product 
+        document.getElementById("quote").innerHTML = price2
         document.getElementById("buy").disabled = false;
         document.getElementById("sell").disabled = false;
     }
 }
 function dropQuote()
 {
-    document.getElementById("quote").innerHTML = 'No Active Quote'
+    document.getElementById("qproduct").innerHTML = ' '
+    document.getElementById("quote").innerHTML = ' '
     console.log('dropped quote')
+}
+function positionAdd() {
+    console.log('updating positions')
+    var table = document.getElementById("positions");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = 'Buy';
+    cell2.innerHTML = document.getElementById("qproduct").innerHTML;
+    cell3.innerHTML = document.getElementById("quote").innerHTML;
 }
