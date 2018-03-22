97ad7784f7b94c1833a25192dc8ce4532f1aeedf
kenneththomas <kennethwoo11@gmail.com>
2018-03-21 22:52:37 -0400

    bob the builder can we fix it? bob the builder yes we can

--- quoter/script.js
+++ quoter/script.js
@@ -8,14 +8,14 @@ function quoteTest(product, baseprice)
         document.getElementById("buy").disabled = true;
         document.getElementById("sell").disabled = true;
     } else {
-        var volatility = parseFloat(Math.random().toFixed(2))
+        var volatility = parseFloat(Math.random())
         if (Math.random() >= 0.5){
             var price2 = baseprice + volatility
             } else {
                 var price2 = baseprice - volatility
                 }
         document.getElementById("qproduct").innerHTML = product 
-        document.getElementById("quote").innerHTML = price2
+        document.getElementById("quote").innerHTML = price2.toFixed(2)
         document.getElementById("buy").disabled = false;
         document.getElementById("sell").disabled = false;
     }
@@ -26,9 +26,9 @@ function dropQuote()
     document.getElementById("quote").innerHTML = ' '
     console.log('dropped quote')
 }
-function buyOrder() {
-    var orderprice = document.getElementById("quote").innerHTML;
-    if (document.getElementById("quote").innerHTML > 0){
+function order(side) {
+    var orderprice = document.getElementById("quote").innerHTML
+    if (orderprice > 0){
         console.log('updating positions')
         var table = document.getElementById("positions");
         var row = table.insertRow(1);
@@ -37,33 +37,14 @@ function buyOrder() {
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
-        cell1.innerHTML = 'Buy';
+        cell1.innerHTML = side;
         cell2.innerHTML = document.getElementById("qproduct").innerHTML;
-        cell3.innerHTML = document.getElementById("quote").innerHTML;
+        cell3.innerHTML = orderprice;
         dropQuote()
         //using 100 quantity until we put in a quantity feature
         cell4.innerHTML = 100
-        cell5.innerHTML = 100 * orderprice
-    }
-}
-function sellOrder() {
-    var orderprice = document.getElementById("quote").innerHTML;
-    if (document.getElementById("quote").innerHTML > 0){
-        console.log('updating positions')
-        var table = document.getElementById("positions");
-        var row = table.insertRow(1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        cell1.innerHTML = 'Buy';
-        cell2.innerHTML = document.getElementById("qproduct").innerHTML;
-        cell3.innerHTML = document.getElementById("quote").innerHTML;
-        dropQuote()
-        //using 100 quantity until we put in a quantity feature
-        cell4.innerHTML = 100
-        cell5.innerHTML = 100 * orderprice
+        //javascript sucks
+        cell5.innerHTML = (100 * orderprice).toFixed(2)
     }
 }
 
