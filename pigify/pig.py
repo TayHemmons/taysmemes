3d213a96704a0fabf3e7b036488afd4ae233c618
kenneththomas <kennethwoo11@gmail.com>
2018-04-19 23:52:51 -0400

    sentence

--- pigify/pig.py
+++ pigify/pig.py
@@ -5,3 +5,11 @@ def pigify(word):
     word = ''.join(word) # make this a string again
     pigword = word + firstletter + 'ay'
     return pigword
+
+def pigsentence(sentence):
+    newsentence=[] # blank list for new sentence
+    words = sentence.split() # split sentence into list
+    for word in words: # for each word in the sentence, pigify
+        newsentence.append(pigify(word))
+    newsentence = ' '.join(newsentence) # turn list back into sentence
+    return newsentence
--- pigify/test.py
+++ pigify/test.py
@@ -3,13 +3,23 @@ import pig
 
 class pigtest(unittest.TestCase):
 
-    def test_fixbasic(self):
+    def test_pigbasic(self):
         #turn a word that starts with one consonant into its pig latin form
         word = 'bridge'
         print('starting word: ' + word)
         pigword = pig.pigify(word)
         print('translated word: ' + pigword)
         self.assertEqual(pigword, 'ridgebay')
+
+    def test_sentence(self):
+        #turn multiple words into pig latin
+        sentence = 'we need more food'
+        print('starting sentence: ' + sentence)
+        pigsentence = pig.pigsentence(sentence)
+        print('translated sentence: ' + pigsentence)
+        self.assertEqual(pigsentence, 'eway eednay oremay oodfay')
+
+
 
 if __name__ == '__main__':
     unittest.main()
