bca8334346d0bd14237e6dbeef96744c5fb69d35
taylor <tayhemmons@protonmail.com>
2019-10-08 19:57:13 -0400

    deconstruct and reconstruct so we can change words more granurarry

--- idpol/idpol.py
+++ idpol/idpol.py
@@ -4,14 +4,25 @@ def ltor(words):
     return words.replace('l','r')
 
 def ruleset1(words):
-    words = words.replace('ie','ee')
-    words = words.replace('ble','bur')
+    newsentence = []
+    words = words.replace('.','')
+    words = words.rstrip()
+    for word in words.split(' '):
+        if word != 'die':
+            word = word.replace('ie','ee')
+        word = word.replace('ble','bur')
+        if word == 'the': # this one is a full word replace
+            word = word.replace('the','da')
+        newsentence.append(word)
+        words = reconstruct(newsentence)
     return words
 
-def ruleset2(words):
-    # all full word replace
-    words = words.replace('the','da')
-    return words
+def reconstruct(wordlist): # takes a list and turns it into a string
+    sentence = ''
+    for word in wordlist:
+        sentence = sentence + ' ' + word
+    sentence = sentence + '.'
+    return sentence
 
 def dumpling(words):
     words = ruleset1(words)
