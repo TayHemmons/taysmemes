f241b58ff8365dc14b97656e922a483edcccf375
taylor <tayhemmons@protonmail.com>
2019-10-10 22:14:02 -0400

    dont look at me

--- idpol/idpol_sample.py
+++ idpol/idpol_sample.py
@@ -6,5 +6,7 @@ biblefile = open('thebible.txt')
 
 bible = biblefile.readlines()
 
+biburr = open('generated/biburr.txt','w')
 for line in bible:
     print(idpol.dumpling(line.lower()))
+    biburr.write(line)
